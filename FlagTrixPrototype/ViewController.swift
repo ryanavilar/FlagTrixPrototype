16520f6987410cc9371f4005fb6e0527b50aa2eb
Erica Liu <ericayijie@gmail.com>
2015-05-04 20:03:54 +1000

    add circle
    circle

--- FlagTrixPrototype/ViewController.swift
+++ FlagTrixPrototype/ViewController.swift
@@ -10,12 +10,67 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController,CLLocationManagerDelegate {
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
     
     var currentLocation : CLLocationCoordinate2D!
+
+    var locationManager: CLLocationManager!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //predefined location
+        locationManager = CLLocationManager()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+
+        addRadiusCircle(location)
+    }
+    func addRadiusCircle(location:CLLocation){
+        self.mapView.delegate = self
+        var circle = MKCircle(centerCoordinate: location.coordinate, radius: 700 as CLLocationDistance)
+        self.mapView.addOverlay(circle)
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKCircle {
+            var circle = MKCircleRenderer(overlay: overlay)
+            circle.strokeColor = UIColor.redColor()
+            circle.fillColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.02)
+            
+            circle.lineWidth = 0.5
+            return circle
+        } else {
+            return nil
+        }
+    }
     @IBAction func startButton(sender: AnyObject) {
         // Add an annotation
         var newYorkLocation = CLLocationCoordinate2DMake(-27.4860878, 152.9926336)
@@ -27,33 +82,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         
         
     }
-    
-    var locationManager: CLLocationManager!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        let location = locations.last as! CLLocation
-        
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
-        self.mapView.setRegion(region, animated: true)
-        let rad = CLLocationDistance(700)
-        MKCircle(centerCoordinate: center, radius: rad)
-    }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
