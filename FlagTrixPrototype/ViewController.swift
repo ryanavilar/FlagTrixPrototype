0a3a7bc7128a60c0eac04421c0ba369974d2c6d1
Erica Liu <ericayijie@gmail.com>
2015-05-25 17:32:59 +1000

    get rid of conflicts

--- FlagTrixPrototype/ViewController.swift
+++ FlagTrixPrototype/ViewController.swift
@@ -109,9 +109,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        
+
         
         let location = locations.last as CLLocation
+
         
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
